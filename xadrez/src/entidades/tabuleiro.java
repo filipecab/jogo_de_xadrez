b41f813cf4f8a66fc93b92b591794dac5c10093d
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-08-31 12:09:51 -0300

    aplicando exceções

--- xadrez/src/entidades/tabuleiro.java
+++ xadrez/src/entidades/tabuleiro.java
@@ -1,10 +1,17 @@
 package entidades;
 
+import javax.swing.text.Position;
+
+import exception.excp_tab;
+
 public class tabuleiro {
     private int linhas;
     private int colunas;
     private peca[][] pecas;
     public tabuleiro(int linhas, int colunas) {
+        if (linhas<1||colunas<1){
+            throw new excp_tab("Erro ao criar tabuleiro deve haver pelo menos 1 linha e  coluna");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas=new peca[linhas][colunas];
@@ -12,26 +19,45 @@ public class tabuleiro {
     public int getLinhas() {
         return linhas;
     }
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
-    }
+   
     public int getColunas() {
         return colunas;
     }
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
+   
     
     public peca pecas(int linha, int coluna){
+        if (!posicaoExist1(linha, coluna)){
+            throw new excp_tab("Posição não existe no tabuleiro");
+        }
         return pecas[linha][coluna];
     }
-    public peca pecas(position pos){
+    public peca peca1(position pos){
+        if (!posicaoExist(pos)){
+            throw new excp_tab("Posição não existe no tabuleiro");
+        }
             return pecas[pos.getLinha()][pos.getColuna()];
     }
     public void Inserepeca(peca p,position ps){
+        if (temPecaPos(ps)){
+            throw new excp_tab("ja existe peça nessa posição "+ps);
+        }
         pecas[ps.getLinha()][ps.getColuna()]=p;
         p.posicao=ps;
     }
+    public boolean posicaoExist1(int linha,int coluna){
+        return linha>=0 && linha<linhas && coluna>=0 && coluna<colunas;
+    }
+
+    public  boolean posicaoExist(position p){
+        return posicaoExist1(p.getLinha(), p.getColuna());
+    }
+    public boolean temPecaPos(position p){
+        if (!posicaoExist(p)){
+            throw new excp_tab("Posição não existe no tabuleiro");
+        }
+        return peca1(p)!=null;
+    }
+
     //public peca removePeca(position p){//
 
 }
---
+++ xadrez/src/exception/excp_tab.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class excp_tab extends RuntimeException {
+    private static final long serialVersionUID=1L;
+
+    public excp_tab(String msg){
+        super(msg);
+    }
+}
