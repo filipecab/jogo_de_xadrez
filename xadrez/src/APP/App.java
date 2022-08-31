77ca709ffed1ad339a91d5e0b85215c127180cdb
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-08-31 17:15:49 -0300

    tratando excecoes na main

--- xadrez/src/APP/App.java
+++ xadrez/src/APP/App.java
@@ -1,11 +1,13 @@
 package APP;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import chessenums.chessPosicao;
 import chessenums.interfac;
 import chessenums.partida;
 import chessenums.pecaxadrez;
+import exception.excp;
 
 
 public class App {
@@ -14,17 +16,28 @@ public class App {
 
         partida part=new partida();
         while (true){
-            interfac.printab(part.getPecas());        
-            System.out.println();
-            System.out.print("Origem: ");
-            chessPosicao origem=interfac.leituraPosicao(sc);
-            System.out.println();
-            System.out.print("Destino: ");
-            chessPosicao des=interfac.leituraPosicao(sc);
+            try{
+                interfac.clearScreen();
+                interfac.printab(part.getPecas());        
+                System.out.println();
+                System.out.print("Origem: ");
+                chessPosicao origem=interfac.leituraPosicao(sc);
+                System.out.println();
+                System.out.print("Destino: ");
+                chessPosicao des=interfac.leituraPosicao(sc);
 
-            pecaxadrez capturar=part.moverPecas(origem,des);
-
+                pecaxadrez capturar=part.moverPecas(origem,des);
             
+            }
+            catch(excp e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                System.out.println("Aperte enter");
+                sc.nextLine();
+            }
         }
     }
 }
